7923c980b6a319c8a8078b403849071d2f6a7c2f
wcgcg <wcgcg@naver.com>
2018-08-04 11:50:11 +0900

    MS

--- test1.js
+++ test1.js
@@ -6,4 +6,24 @@ if(input % 2 == 0) {
 
 if(input % 2 == 1) {
     console.log("홀수입니다!");
+        }
+
+        if(intput % 2 == 0){
+            console.log("짝수!!!")
+        }
+        else{
+            console.log("홀수!!!")
+        }
+        
+        
+switch(input % 2){
+    case 0:
+        console.log("짝수!!!")
+        break;
+
+    case 1:
+        console.log("짝수!!!")
+        break;
 }
+
+(input % 2 == 0) ? "짝수" : "홀수";
---
+++ test_array.js
@@ -0,0 +1,16 @@
+let array = [52, 273, '아침밥', '점심밥', true, false]
+
+
+console.log(array);
+
+array[0] = 0;
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+
+for (i=0; i<6; i++) {
+    console.log(array[i]);
+}
